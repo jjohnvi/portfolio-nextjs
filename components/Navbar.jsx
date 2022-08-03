f5ac3b0a03621a712161ecad650e59107a517494
John Jeong <jjohnvi@Johns-MBP.lan>
2022-08-03 16:18:18 -0500

    added navbar visible and making responsive

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,8 +1,31 @@
+import Image from 'next/image';
 import React from 'react';
+import Link from 'next/link';
+import { AiOutlineClose, AiOutlineMenu} from 'react-icons/ai';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='fixed w-full h-20 shadow-xl z-[100]'>
+      <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
+        <Image src="" alt="" width='125' height='50' />
+      <div>
+        <ul className='hidden md:flex'>
+          <Link href='/'><li className="ml-10 text-sm uppercase hover:border-b">Home</li></Link>
+          <Link href='/'><li className="ml-10 text-sm uppercase hover:border-b">About</li></Link>
+          <Link href='/'><li className="ml-10 text-sm uppercase hover:border-b">Skills</li></Link>
+          <Link href='/'><li className="ml-10 text-sm uppercase hover:border-b">Projects</li></Link>
+          <Link href='/'><li className="ml-10 text-sm uppercase hover:border-b">Contact</li></Link>
+        </ul>
+        <div className='md:hidden'>
+          <AiOutlineMenu size={25} />
+        </div>
+      </div>
+      </div>
+
+      <div className='fixed left-0 top-0 w-full h-screen bg-black/70'></div>
+      <div className='fixed left-0 top-0 w-[75%] sm:[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500'></div>
+
+    </div>
   )
 }
 
