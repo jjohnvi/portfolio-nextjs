c9a53d17053c3c9dc60006d2e78bd9f4225d71a1
John Jeong <jjohnvi@Johns-MBP.lan>
2022-08-03 17:32:47 -0500

    sidebar wip

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
   return (
     <div className='fixed w-full h-20 shadow-xl z-[100]'>
       <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
-        <Image src="" alt="" width='125' height='50' />
+        <Image src='/../public/assets/Peep-inverted.svg' alt='' width='125' height='50' />
       <div>
         <ul className='hidden md:flex'>
           <Link href='/'><li className="ml-10 text-sm uppercase hover:border-b">Home</li></Link>
@@ -22,9 +22,18 @@ const Navbar = () => {
       </div>
       </div>
 
-      <div className='fixed left-0 top-0 w-full h-screen bg-black/70'></div>
-      <div className='fixed left-0 top-0 w-[75%] sm:[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500'></div>
-
+      <div className='fixed left-0 top-0 w-full h-screen bg-black/70'>
+      <div className='fixed left-0 top-0 w-[75%] sm:[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500'>
+      <div>
+        <div className='flex w-full items-center justify-between'>
+        <Image src='/../public/assets/Peep-inverted.svg' width='87' height='35' alt='/' />
+        <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer'>
+          <AiOutlineClose />
+        </div>
+        </div>
+        </div>
+      </div>
+      </div>
     </div>
   )
 }
