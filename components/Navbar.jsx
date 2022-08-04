271072b7ff587dd83d98a1c543c8ca338afeaa16
John Jeong <jjohnvi@Johns-MBP.lan>
2022-08-03 21:51:23 -0500

    Worked on main page, adding some content and design

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,11 +1,20 @@
 import Image from 'next/image';
-import React from 'react';
+import React, {useState} from 'react';
 import Link from 'next/link';
 import { AiOutlineClose, AiOutlineMenu, AiOutlineMail} from 'react-icons/ai';
 import {FaLinkedinIn, FaGithub, } from 'react-icons/fa';
 import {BsFillPersonLinesFill} from 'react-icons/bs';
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+
+  
+  const handleNav = () => {
+    setNav(!nav);
+  }
+  
+
+
   return (
     <div className='fixed w-full h-20 shadow-xl z-[100]'>
       <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
@@ -18,18 +27,19 @@ const Navbar = () => {
           <Link href='/'><li className="ml-10 text-sm uppercase hover:border-b">Projects</li></Link>
           <Link href='/'><li className="ml-10 text-sm uppercase hover:border-b">Contact</li></Link>
         </ul>
-        <div className='md:hidden'>
+        <div onClick={handleNav} className='md:hidden'>
           <AiOutlineMenu size={25} />
         </div>
       </div>
       </div>
 
-      <div className='fixed left-0 top-0 w-full h-screen bg-black/70'>
-      <div className='fixed left-0 top-0 w-[75%] sm:[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500'>
+      <div className={nav ? 'md:hidden fixed left-0 top-0 w-full h-screen bg-black/70' : ''}>
+      <div className={nav ? 'fixed left-0 top-0 w-[75%] sm:[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500' 
+      : 'fixed left-[-100%] top-0 p-10 ease-in duration-500'}>
       <div>
         <div className='flex w-full items-center justify-between'>
         <Image src='/../public/assets/Peep-inverted.svg' width='87' height='35' alt='/' />
-        <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer'>
+        <div onClick={handleNav} className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer'>
           <AiOutlineClose />
         </div>
         </div>
@@ -76,6 +86,6 @@ const Navbar = () => {
       </div>
     </div>
   )
-}
+  }
 
 export default Navbar;
